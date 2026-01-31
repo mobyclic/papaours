ed42a0ad749d396f02e98b7c2946135254ebab5d
Alistair Marca <alistair.marca@gmail.com>
2026-01-31 19:36:35 +0100

    kwizy

--- src/routes/api/stripe/webhook/+server.ts
+++ src/routes/api/stripe/webhook/+server.ts
@@ -4,7 +4,7 @@ import Stripe from 'stripe';
 import { env } from '$env/dynamic/private';
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY || '', {
-  apiVersion: '2025-01-27.acacia'
+  apiVersion: '2026-01-28.clover'
 });
 
 export const POST: RequestHandler = async ({ request }) => {
